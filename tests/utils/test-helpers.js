89ffa29968d6587a9aa6419478a58c8ffcbf8c00
Carl Rannaberg <carlrannaberg@gmail.com>
2025-08-06 22:20:16 +0300

    fix: use .js extension in test imports for ESM compatibility

--- tests/utils/test-helpers.js
+++
@@ -1,4 +0,0 @@
-// ESM re-export wrapper for TypeScript module
-// This file exists to bridge ESM module resolution in environments that don't fully support TypeScript
-// It allows importing '../utils/test-helpers' without the .js extension
-export * from './test-helpers.ts';
